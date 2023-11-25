822639a6a6942165a7d90c418d0eaf3a16b48302
D <jayashree.a.d@capgemini.com>
2023-11-25 21:45:35 +0530

    Initial commit

---
+++ src/main/java/net/javaguides/springboot/entity/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package net.javaguides.springboot.entity.repository;
+
+import net.javaguides.springboot.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
